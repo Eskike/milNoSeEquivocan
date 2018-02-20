31912e6582b041eb27bd365c184da8c20b9f4cc3
Sergiogmbg <sergiog.mbg@gmail.com>
2018-02-20 13:05:11 +0100

    Cambios:
    - Mejoras en la actividad de login (Inclusión de icono y mayor tamaño de los elementos)
    - Botón Iniciar (de la actividad login) manda a Selección de vino

--- app/src/main/java/es/alcazardesanjuan/milnoseequivocan/MainActivity.java
+++ app/src/main/java/es/alcazardesanjuan/milnoseequivocan/MainActivity.java
@@ -8,16 +8,23 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button bInfo;
+    Button bIniciar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bInfo = (Button) findViewById(R.id.buttonInfo);
+        bIniciar = (Button) findViewById(R.id.buttonIniciar);
 
     }
     public void bInfo (View view){
         Intent activity=new Intent(getApplicationContext(),activity_aboutus.class);
+        startActivity(activity);
+    }
+
+    public void bIniciar (View view){
+        Intent activity=new Intent(getApplicationContext(),WineSelection.class);
         startActivity(activity);
     }
 }
