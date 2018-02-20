5eee2678263a42bb4a01ed61a6b33a91c6399acb
Svanhild42 <izumi_scarlet@hotmail.com>
2018-02-20 12:28:47 +0100

    Acción para pasar activities de activity_main.xml a activity_aboutus.xml Cambios en el Java de MainActivity para añadir método y función de cambiar entre actividades.

--- app/src/main/java/es/alcazardesanjuan/milnoseequivocan/MainActivity.java
+++ app/src/main/java/es/alcazardesanjuan/milnoseequivocan/MainActivity.java
@@ -1,13 +1,23 @@
 package es.alcazardesanjuan.milnoseequivocan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button bInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bInfo = (Button) findViewById(R.id.buttonInfo);
+
+    }
+    public void bInfo (View view){
+        Intent activity=new Intent(getApplicationContext(),activity_aboutus.class);
+        startActivity(activity);
     }
 }
