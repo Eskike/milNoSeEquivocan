95065887f9dae3edf12b927707970ff20f1cea51
Sergiogmbg <sergiog.mbg@gmail.com>
2018-02-20 12:13:10 +0100

    Cambios en:
    - Pequeños cambios en la actividad de Login
    - Creada la actividad de selección de vino
    - Pequeños cambios en los estilos

---
+++ app/src/main/java/es/alcazardesanjuan/milnoseequivocan/WineSelection.java
@@ -0,0 +1,13 @@
+package es.alcazardesanjuan.milnoseequivocan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WineSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wine_selection);
+    }
+}
